170c6f14e2165c91df199b35c6a13658bd32730e
Patrick <pscampos221@gmail.com>
2023-08-17 17:55:46 -0300

    Melhorias na filtragem de Palavras

--- GarticGuess.py
+++ GarticGuess.py
@@ -1,85 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
+import time
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class GarticGuess:
     def __init__(self):
+        print("Gartic.io Bot iniciando...")
         self.lines = None
         self.load_lines()
         self.options = webdriver.ChromeOptions()
         self.options.page_load_strategy = 'none'
-        self.options.add_argument("user-data-dir={}\driver_data".format(os.getcwd()))
+        self.options.add_argument(
+            "user-data-dir={}\driver_data".format(os.getcwd()))
         self.driver = webdriver.Chrome(options=self.options)
         self.driver.get("https://gartic.io/")
         self.is_game = False
         self.wait = WebDriverWait(self.driver, 10)
+        print("Aguardando carregamento da página...")
 
     def load_lines(self):
         self.lines = open("words.txt", "r", encoding="utf8").readlines()
         self.lines = [line.strip() for line in self.lines]
         self.lines.sort()
-        
+
         with open("words.txt", "w", encoding="utf8") as f:
             for line in self.lines:
                 f.write(line + "\n")
 
-    def extract_words(self, length, letters):
-        word_list = []
-        for line in self.lines:
-            if len(line) == length and all(letter in line for letter in letters) and len(letters) > 0 and line[0] == letters[0]:
-                word_list.append(line)
-                
-        word_list = list(set(word_list))
-        word_list.sort()
-                
-        return word_list
-
-    def run(self):
-        while True:
-            try:
-                self.driver.find_element(By.ID, 'possible_words')
-            except NoSuchElementException:
-                self.create_possible_words_div()
-
-            try:
-                answer_input = self.driver.find_element(By.XPATH, "//input[@name='answer']")
-                if not self.is_game:
-                    if answer_input:
-                        print("Jogo encontrado, carregando componentes de palavras...")
-                        self.is_game = True
-
-                if self.is_game:
-                    hint = self.driver.find_element(By.ID, 'hint')
-                    if hint:
-                        #print("Dica encontrada, extraindo palavras...")
-                        words_divs = self.driver.find_elements(By.CLASS_NAME, 'word')
-                        #print(f"{len(words_divs)} sílabas encontradas")
-                        
-                        separated_letters_without_hyphen = [span.text.lower() for word_div in words_divs for span in word_div.find_elements(By.TAG_NAME, 'span')]
-                        complete_word = ''.join(separated_letters_without_hyphen).replace('-', '')
-
-                        possible_words = self.extract_words(len(separated_letters_without_hyphen), complete_word)
-                        #print(f"Palavras possíveis sem filtro ({len(possible_words)}): {possible_words}")
-
-                        filtered_words = []
-                        for word in possible_words:
-                            matching_indices = [i for i, letter in enumerate(word) if letter == separated_letters_without_hyphen[i]]
-                            if matching_indices == list(range(len(matching_indices))):
-                                filtered_words.append(word)
-
-                        #print(f"Palavras possíveis após filtragem ({len(filtered_words)}): {filtered_words}")
-
-                        best_words = [word for word in possible_words if word not in filtered_words] if len(filtered_words) < len(possible_words) else filtered_words
-
-                        self.update_possible_words_div(best_words)
-
-            except (NoSuchElementException, ElementNotInteractableException):
-                pass
+        print(f"{len(self.lines)} palavras carregadas com sucesso!")
 
     def create_possible_words_div(self):
+        if self.driver.find_elements(By.ID, 'possible_words'):
+            return
         possible_words_div = self.driver.execute_script(
             "var div = document.createElement('div');"
             "div.id = 'possible_words';"
@@ -97,12 +53,58 @@ class GarticGuess:
             "document.body.appendChild(div);"
             "return div;"
         )
-        self.wait.until(EC.presence_of_element_located((By.ID, 'possible_words')))
+        self.wait.until(EC.presence_of_element_located(
+            (By.ID, 'possible_words')))
+
+    def extract_words(self, length, letters):
+        word_list = []
+        for line in self.lines:
+            if len(line) == length and all(letter in line for letter in letters) and len(letters) > 0 and line[0] == letters[0]:
+                word_list.append(line)
+
+        word_list = list(set(word_list))
+        word_list.sort()
+
+        return word_list
+
+    def run(self):
+        while True:
+            self.create_possible_words_div()
+
+            if not self.is_game:
+                if self.driver.find_elements(
+                        By.XPATH, "//input[@name='answer']"):
+                    print("Jogo encontrado, carregando componentes de palavras...")
+                    self.is_game = True
+
+            if self.is_game:
+                if self.driver.find_elements(By.ID, 'hint') and self.driver.find_elements(
+                        By.CLASS_NAME, 'word'):
+                    time.sleep(1)
+                    separated_letters = [span.text.lower(
+                    ) for word_div in self.driver.find_elements(
+                        By.CLASS_NAME, 'word') for span in word_div.find_elements(By.TAG_NAME, 'span')]
+                    complete_word = ''.join(
+                        separated_letters)
+
+                    possible_words = self.extract_words(
+                        len(separated_letters), complete_word)
+
+                    filtered_words = []
+
+                    for word in possible_words:
+                        lower_word = word.lower()
+                        if all(lower_word[i] == separated_letters[i] or separated_letters[i] == '' for i in range(len(lower_word))):
+                            filtered_words.append(word)
+                    self.update_possible_words_div(filtered_words)
 
     def update_possible_words_div(self, words):
-        possible_words_list_html = "<ul>" + "".join(f"<li>{word}</li>" for word in words) + "</ul>"
+        possible_words_list_html = "<ul>" + \
+            "".join(f"<li>{word}</li>" for word in words) + "</ul>"
         possible_words_div = self.driver.find_element(By.ID, 'possible_words')
-        self.driver.execute_script("arguments[0].innerHTML = arguments[1];", possible_words_div, possible_words_list_html)
+        self.driver.execute_script(
+            "arguments[0].innerHTML = arguments[1];", possible_words_div, possible_words_list_html)
+
 
 if __name__ == "__main__":
     solver = GarticGuess()
